8e7d8b93c778b882a3aa5917f37b6b91d47d11fc
Mark Bergman (aka xychix) <xychix@hotmail.com>
2019-04-14 08:57:32 +0200

    Prepare for lazy running, we're not DevOps ;)

--- redFile.py
+++ redFile.py
@@ -53,4 +53,5 @@ def staticlst():
         return(html)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=18080,debug=True)
+    # DEBUG app.run(host='0.0.0.0',port=18080,debug=True)
+    app.run(host='127.0.0.1',port=18080,debug=False)
